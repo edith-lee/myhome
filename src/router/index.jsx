f6b39a714d63aeff1f3413ad3efd9bc6aa428813
liyixun <834319745@qq.com>
2019-10-28 10:26:02 +0800

    first commit

---
+++ src/router/index.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+import {Switch,HashRouter as Router,Route} from 'react-router-dom'
+
+export default class MyIndex  extends Component{
+    render(){
+        return(
+                <Router>
+                    <Switch>
+                        
+                    </Switch>
+                </Router>
+        )
+    }
+}
